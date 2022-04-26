c8fb489e49203eb811ffdcd9a2a03da210ff3fdd
gm2552 <meyerg@vmware.com>
2022-04-26 08:10:26 -0500

    Initial commit.

---
+++ src/main/java/org/nhindirect/smtpsink/DirectSmtpSinkApplication.java
@@ -0,0 +1,13 @@
+package org.nhindirect.smtpsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectSmtpSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectSmtpSinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nhindirect/smtpsink/DirectSmtpSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhindirect.smtpsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectSmtpSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
